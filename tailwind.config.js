be218df8ffc35121814228babdc2c61df8fd0b5e
Success <stealph14@gmail.com>
2025-07-01 16:59:30 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./*.{html,js}"],
+   theme: {
+    screens: {
+        sm: "340px",
+        md: "540px",
+        lg: "768px",
+        xl: "1180px"
+    },
+     extend: {},
+     fontFamily: {
+       Jost: ["Jost", "sans-serif"],
+       Lobster: ["Lobster", "sans-serif"]
+     }, 
+      container: {
+       center: true,
+       padding: {
+        DEFAULT: "12px",
+        md: "32px",
+       }
+      }
+   },
+   plugins: [],
+ }
