6e12de03efafc016be15c193f4e849f192f53412
Successdanesy <successdanesy@gmail.com>
2025-07-05 13:52:04 +0100

    finished home section, services section next

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,26 @@ export default {
         xl: "1180px"
     },
      extend: {},
+     keyframes: {
+        move: {
+          "50%": { transform: "translateY(-1rem)" }
+        },
+        rotate: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        scaleup: {
+          "0%": { transform: "scale(0.8)" },
+          "50%": { transform: "scale(1.2)" },
+          "100%": { transform: "scale(0.8)" },
+
+        }
+     },
+      animation: {
+          movingY: "move 3s linear infinite",
+          rotating: "rotate 15s linear infinite",
+          scaleup: "scaleup 3s linear infinite",
+      },
      fontFamily: {
        Jost: ["Jost", "sans-serif"],
        Lobster: ["Lobster", "sans-serif"]
