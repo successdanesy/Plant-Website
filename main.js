222432f3a27df808d13f054f44a2a6d3e6549b12
Successdanesy <successdanesy@gmail.com>
2025-07-28 17:02:14 +0100

    1:16:07/1:44:29

--- main.js
+++ main.js
@@ -14,3 +14,32 @@ navLinks.forEach(link => {
         hamburger.classList.toggle("ri-close-large-line")
     })
 })
+
+// SWIPER
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    }
+});
