fa85f4f13cc260b4ba9108e993014ef05115cb63
Successdanesy <successdanesy@gmail.com>
2025-07-29 16:59:36 +0100

    1:37:03/ 1:44:29

--- main.js
+++ main.js
@@ -15,6 +15,34 @@ navLinks.forEach(link => {
     })
 })
 
+/*SCROLL UP*/
+const scrollUp = () => {
+const scrollUpBtn = document.getElementById("scroll-up")
+
+if(this.scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2")
+    scrollUpBtn.classList.add("bottom-4")
+} else {
+    scrollUpBtn.classList.add("-bottom-1/2")
+    scrollUpBtn.classList.remove("bottom-4")
+}
+}
+
+window.addEventListener("scroll", scrollUp)
+
+/*CHANGE BACKGROUND HEADER */
+const scrollHeader = () => {
+const header = document.getElementById("navbar")
+
+if(this.scrollY >= 250) {
+    header.classList.add("border-b", "border-yellow-500")
+} else {
+    header.classList.remove("border-b", "border-yellow-500")
+}
+}
+
+window.addEventListener("scroll", scrollHeader)
+
 // SWIPER
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -43,3 +71,43 @@ const swiper = new Swiper('.swiper', {
         },
     }
 });
+
+/*scroll sections active link */
+const activeLink = () => {
+    const sections = document.querySelectorAll('section')
+    const navLinks = document.querySelectorAll('.nav-link')
+
+    let current = "home"
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if(this.scrollY >= sectionTop -60) {
+            current = section.getAttribute("id")
+        }
+    })
+
+    navLinks.forEach(item => {
+        item.classList.remove("active")
+
+        if(item.href.includes(current)) {
+            item.classList.add("active")
+        }
+    })
+}
+
+window.addEventListener("scroll", activeLink)
+
+/*scroll reveal */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    reset: true
+})
+
+sr.reveal('.home__data')
+sr.reveal('.home__image', {delay: 500, scale: 0.5})
+
+sr.reveal('.service__card', {interval: 100})
